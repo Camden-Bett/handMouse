80f89c9dc90811c1bd46735da9f5d74ce80be37c
Camden Bettencourt <camden.r.bettencourt@gmail.com>
2024-02-17 10:40:19 -0500

    Created actual venv

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #say hello
+    print_hi('Cam')
+
